64154c77eb602e18c1cb6b3962e5b6cee3f3120c
sekonioluwaseun@gmail.com <sekonioluwaseun@gmail.com>
2019-07-29 11:36:51 +0100

    Created default user and andmin user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,21 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin Admin',
-            'email' => 'admin@argon.com',
+            'name' => 'User',
+            'email' => 'user@postget.com',
+            'role_id' => 0,
             'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@postget.com',
+            'role_id' => 1,
+            'email_verified_at' => now(),
+            'password' => Hash::make('adminpassword'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
